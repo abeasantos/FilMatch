03837ada4aa2f7b77cacb57d4cdadd8d80d5a5ee
Bruno Souza <bruno1994deilson@gmail.com>
2022-03-07 22:16:15 -0300

    add endpoint suporte e movies

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var suporteRouter = require('./routes/suporte');
+var moviesRouter = require('./routes/movies');
+var sobrenosRouter = require('./routes/sobrenos');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/suporte', suporteRouter);
+app.use('/movies', moviesRouter);
+app.use('/sobrenos', sobrenosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
